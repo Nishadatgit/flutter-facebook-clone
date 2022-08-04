63996833d04f304f653cf1c21f321095417e078c
Nishadatgit <nishadns321@gmail.com>
2022-08-04 20:32:26 +0530

    project outputs added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -138,9 +138,7 @@ class _HomeScreenDesktop extends StatelessWidget {
       children: [
         Flexible(
           flex: 2,
-          child: Container(
-            color: Colors.orange,
-          ),
+          child: Container(),
         ),
         const Spacer(),
         SizedBox(
